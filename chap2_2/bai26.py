ed43302b5510985a661d683c57abe765187ab22c
hviet93 <3d1080@gmail.com>
2021-04-17 15:53:43 +0700

    Bai 26 part 1

---
+++ chap2_2/bai26.py
@@ -0,0 +1,44 @@
+import openpyxl
+
+wb_obj = openpyxl.load_workbook('./../data_sample/data_2_2/marathon.xlsx')
+sheet_obj = wb_obj.active
+m_row = sheet_obj.max_row
+data_marathon = []
+stem_leaf_marathon = {}
+
+
+def import_data_array():
+    for i in range(2, m_row + 1):
+        cell_obj = sheet_obj.cell(row=i, column=1)
+
+        if cell_obj.value is not None:
+            data_marathon.append(cell_obj.value)
+
+
+def show_stem_leaf_char():
+    global stem_leaf_marathon
+
+    for val in data_marathon:
+        stem_val = int(val / 10)
+        leaf_val = val - stem_val * 10
+
+        if str(stem_val) not in stem_leaf_marathon:
+            stem_leaf_marathon[str(stem_val)] = [leaf_val]
+        else:
+            stem_leaf_marathon[str(stem_val)].append(leaf_val)
+
+    stem_leaf_marathon = sorted(stem_leaf_marathon.items(), key=lambda x: int(x[0]))
+
+    for stem_leaf in stem_leaf_marathon:
+        stem_leaf_str = ' ' + str(stem_leaf[0]) + ' | ' if int(stem_leaf[0]) < 10 \
+            else str(stem_leaf[0]) + ' | '
+        stem_leaf[1].sort()
+
+        for leaf_val in stem_leaf[1]:
+            stem_leaf_str = stem_leaf_str + ' ' + str(leaf_val)
+
+        print(stem_leaf_str)
+
+
+import_data_array()
+show_stem_leaf_char()
