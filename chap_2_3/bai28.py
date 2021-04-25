9bd4760f2b76ad184b1762f9e4322e53caa013f9
hviet93 <3d1920@hotmail.com.vn>
2021-04-25 17:12:59 +0700

    Bai 28 part 2

--- chap_2_3/bai28.py
+++ chap_2_3/bai28.py
@@ -7,22 +7,39 @@ m_row = sheet_obj.max_row
 data = []
 # x:
 # min: 13, max 84, step: 20
-# 10-29, 30-49, 50-69, 60-79, 80-99
+# 10-29, 30-49, 50-69, 70-89
 # y:
 # min: 21, max: 99, step: 20
-# 40-59, 60-79, 80-99
-data_input = {
-    'X': {
-        '10_29': 0,
-        '30_49': 0,
-        '50_69': 0,
-        '60_79': 0,
-        '80_99': 0,
+# 20-39, 40-59, 60-79, 80-99
+# --------------------------
+# col1: 20_29, 30_39, 0, 0
+# col2: 0, 40_49, 59_60, 0
+# col3: 0, 0, 50_59, 70_79
+# col4: 0, 0, 0, 80_89
+table_input_data = {
+    'col1': {
+        '20_29': 0,
+        '30_39': 0,
+        'row3': 0,
+        'row4': 0
     },
-    'Y': {
-        '40_59': 0,
-        '60_79': 0,
-        '80_99': 0,
+    'col2': {
+        'row1': 0,
+        '40_49': 0,
+        '50_59': 0,
+        'row4': 0
+    },
+    'col3': {
+        'row1': 0,
+        'row2': 0,
+        '60_69': 0,
+        '70_79': 0
+    },
+    'col4': {
+        'row1': 0,
+        'row2': 0,
+        'row3': 0,
+        '80_89': 0
     }
 }
 
@@ -42,30 +59,87 @@ def calc_data():
         lbl_row = val[0]
         lbl_col = val[1]
 
-        for key in data_input['X']:
-            key_val_arr = str(key).split('_')
-            key_val1 = int(key_val_arr[0])
-            key_val2 = int(key_val_arr[1])
+        # Table input data
+        for key in table_input_data['col1']:
+            if 'row' not in str(key):
+                key_val_arr = str(key).split('_')
+                key_val1 = int(key_val_arr[0])
+                key_val2 = int(key_val_arr[1])
 
-            if key_val1 <= lbl_row <= key_val2:
-                data_input['X'][key] += 1
+                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
+                    table_input_data['col1'][key] += 1
 
-        for key in data_input['Y']:
-            key_val_arr = str(key).split('_')
-            key_val1 = int(key_val_arr[0])
-            key_val2 = int(key_val_arr[1])
+        for key in table_input_data['col2']:
+            if 'row' not in str(key):
+                key_val_arr = str(key).split('_')
+                key_val1 = int(key_val_arr[0])
+                key_val2 = int(key_val_arr[1])
 
-            if key_val1 <= lbl_col <= key_val2:
-                data_input['Y'][key] += 1
+                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
+                    table_input_data['col2'][key] += 1
+
+        for key in table_input_data['col3']:
+            if 'row' not in str(key):
+                key_val_arr = str(key).split('_')
+                key_val1 = int(key_val_arr[0])
+                key_val2 = int(key_val_arr[1])
+
+                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
+                    table_input_data['col3'][key] += 1
+
+        for key in table_input_data['col4']:
+            if 'row' not in str(key):
+                key_val_arr = str(key).split('_')
+                key_val1 = int(key_val_arr[0])
+                key_val2 = int(key_val_arr[1])
+
+                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
+                    table_input_data['col4'][key] += 1
 
 
 def show_crosstab_data():
     fig, ax = plt.subplots(1, 1)
     ax.axis('tight')
     ax.axis('off')
-    column_labels = ['', '40-59', '60-79', '80-99', 'Total']
+    column_labels = ['', '20-39', '40-59', '60-79', '80-99', 'Total']
+    row_labels = ['10-29', '30-49', '50-69', '70-89']
     table_data = []
+    list_key_col1 = list(table_input_data['col1'])
+    list_key_col2 = list(table_input_data['col2'])
+    list_key_col3 = list(table_input_data['col3'])
+    list_key_col4 = list(table_input_data['col4'])
+    total_col1 = 0
+    total_col2 = 0
+    total_col3 = 0
+    total_col4 = 0
+
+    for i in range(0, 4):
+        key_col1 = list_key_col1[i]
+        key_col2 = list_key_col2[i]
+        key_col3 = list_key_col3[i]
+        key_col4 = list_key_col4[i]
+        total = table_input_data['col1'][key_col1] + table_input_data['col2'][key_col2] \
+            + table_input_data['col3'][key_col3] + table_input_data['col4'][key_col4]
+        total_col1 += table_input_data['col1'][key_col1]
+        total_col2 += table_input_data['col2'][key_col2]
+        total_col3 += table_input_data['col3'][key_col3]
+        total_col4 += table_input_data['col4'][key_col4]
+        sub_data = [
+            row_labels[i], table_input_data['col1'][key_col1],
+            table_input_data['col2'][key_col2], table_input_data['col3'][key_col3],
+            table_input_data['col4'][key_col4], total
+        ]
+        table_data.append(sub_data)
+    table_data.append([
+        'Total', total_col1, total_col2,
+        total_col3, total_col4, total_col1 + total_col2 + total_col3 + total_col4
+    ])
+
+    ax.table(cellText=table_data, colLabels=column_labels, loc="center")
+    # plt.savefig('frequency.png', dpi=500)
+    plt.show()
 
 
 import_data_array()
 calc_data()
+show_crosstab_data()
