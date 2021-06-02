e7b9d10e7cc6dadf058bf11f88630d099f5ac9b7
hviet93 <3d1080@gmail.com>
2021-06-02 11:03:54 +0700

    Updated doc

--- chap_2_3/bai28.py
+++ chap_2_3/bai28.py
@@ -17,29 +17,29 @@ data = []
 # col3: 0, 0, 50_59, 70_79
 # col4: 0, 0, 0, 80_89
 table_input_data = {
-    'col1': {
-        '20_29': 0,
-        '30_39': 0,
-        'row3': 0,
-        'row4': 0
+    '20_39': {
+        '10_29': 0,
+        '30_49': 0,
+        '50_69': 0,
+        '70_90': 0
     },
-    'col2': {
-        'row1': 0,
-        '40_49': 0,
-        '50_59': 0,
-        'row4': 0
+    '40_59': {
+        '10_29': 0,
+        '30_49': 0,
+        '50_69': 0,
+        '70_90': 0
     },
-    'col3': {
-        'row1': 0,
-        'row2': 0,
-        '60_69': 0,
-        '70_79': 0
+    '60_79': {
+        '10_29': 0,
+        '30_49': 0,
+        '50_69': 0,
+        '70_90': 0
     },
-    'col4': {
-        'row1': 0,
-        'row2': 0,
-        'row3': 0,
-        '80_89': 0
+    '80_100': {
+        '10_29': 0,
+        '30_49': 0,
+        '50_69': 0,
+        '70_90': 0
     }
 }
 
@@ -60,41 +60,20 @@ def calc_data():
         lbl_col = val[1]
 
         # Table input data
-        for key in table_input_data['col1']:
-            if 'row' not in str(key):
-                key_val_arr = str(key).split('_')
-                key_val1 = int(key_val_arr[0])
-                key_val2 = int(key_val_arr[1])
+        for key_col in table_input_data:
+            key_col_val_arr = str(key_col).split('_')
+            key_col_val1 = int(key_col_val_arr[0])
+            key_col_val2 = int(key_col_val_arr[1])
 
-                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
-                    table_input_data['col1'][key] += 1
+            if key_col_val1 <= lbl_col <= key_col_val2:
+                for key_row in table_input_data[key_col]:
+                    key_row_val_arr = str(key_row).split('_')
+                    key_row_val1 = int(key_row_val_arr[0])
+                    key_row_val2 = int(key_row_val_arr[1])
 
-        for key in table_input_data['col2']:
-            if 'row' not in str(key):
-                key_val_arr = str(key).split('_')
-                key_val1 = int(key_val_arr[0])
-                key_val2 = int(key_val_arr[1])
-
-                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
-                    table_input_data['col2'][key] += 1
-
-        for key in table_input_data['col3']:
-            if 'row' not in str(key):
-                key_val_arr = str(key).split('_')
-                key_val1 = int(key_val_arr[0])
-                key_val2 = int(key_val_arr[1])
-
-                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
-                    table_input_data['col3'][key] += 1
-
-        for key in table_input_data['col4']:
-            if 'row' not in str(key):
-                key_val_arr = str(key).split('_')
-                key_val1 = int(key_val_arr[0])
-                key_val2 = int(key_val_arr[1])
-
-                if key_val1 <= lbl_row <= key_val2 or key_val1 <= lbl_col <= key_val2:
-                    table_input_data['col4'][key] += 1
+                    if key_row_val1 <= lbl_row <= key_row_val2:
+                        table_input_data[key_col][key_row] += 1
+                        break
 
 
 def show_crosstab_data():
@@ -104,10 +83,10 @@ def show_crosstab_data():
     column_labels = ['', '20-39', '40-59', '60-79', '80-99', 'Total']
     row_labels = ['10-29', '30-49', '50-69', '70-89']
     table_data = []
-    list_key_col1 = list(table_input_data['col1'])
-    list_key_col2 = list(table_input_data['col2'])
-    list_key_col3 = list(table_input_data['col3'])
-    list_key_col4 = list(table_input_data['col4'])
+    list_key_col1 = list(table_input_data['20_39'])
+    list_key_col2 = list(table_input_data['40_59'])
+    list_key_col3 = list(table_input_data['60_79'])
+    list_key_col4 = list(table_input_data['80_100'])
     total_col1 = 0
     total_col2 = 0
     total_col3 = 0
@@ -118,16 +97,16 @@ def show_crosstab_data():
         key_col2 = list_key_col2[i]
         key_col3 = list_key_col3[i]
         key_col4 = list_key_col4[i]
-        total = table_input_data['col1'][key_col1] + table_input_data['col2'][key_col2] \
-            + table_input_data['col3'][key_col3] + table_input_data['col4'][key_col4]
-        total_col1 += table_input_data['col1'][key_col1]
-        total_col2 += table_input_data['col2'][key_col2]
-        total_col3 += table_input_data['col3'][key_col3]
-        total_col4 += table_input_data['col4'][key_col4]
+        total = table_input_data['20_39'][key_col1] + table_input_data['40_59'][key_col2] \
+                + table_input_data['60_79'][key_col3] + table_input_data['80_100'][key_col4]
+        total_col1 += table_input_data['20_39'][key_col1]
+        total_col2 += table_input_data['40_59'][key_col2]
+        total_col3 += table_input_data['60_79'][key_col3]
+        total_col4 += table_input_data['80_100'][key_col4]
         sub_data = [
-            row_labels[i], table_input_data['col1'][key_col1],
-            table_input_data['col2'][key_col2], table_input_data['col3'][key_col3],
-            table_input_data['col4'][key_col4], total
+            row_labels[i], table_input_data['20_39'][key_col1],
+            table_input_data['40_59'][key_col2], table_input_data['60_79'][key_col3],
+            table_input_data['80_100'][key_col4], total
         ]
         table_data.append(sub_data)
     table_data.append([
@@ -140,6 +119,34 @@ def show_crosstab_data():
     plt.show()
 
 
+def show_row_percentage_data():
+    fig, ax = plt.subplots(1, 1)
+    ax.axis('tight')
+    ax.axis('off')
+    column_labels = ['', '20-39', '40-59', '60-79', '80-99', 'Total']
+    row_labels = ['10-29', '30-49', '50-69', '70-89']
+    list_key_col = list(table_input_data['20_39'])
+    table_data = []
+
+    for key_row in table_input_data:
+        total_row = 0
+        sub_data = []
+        # Count total
+        for i in range(0, 4):
+            key_col = list_key_col[i]
+            total_row = table_input_data[key_row][key_col] + table_input_data[key_row][key_col] \
+                + table_input_data[key_row][key_col] + table_input_data[key_row][key_col]
+            print(total_row)
+
+        # Add data
+        # for i in range(0, 4):
+        #     key_col = list_key_col[i]
+        #     per_row = (table_input_data[key_row][key_col] / total_row) * 100
+
+
+
+
 import_data_array()
 calc_data()
-show_crosstab_data()
+# show_crosstab_data()
+show_row_percentage_data()
